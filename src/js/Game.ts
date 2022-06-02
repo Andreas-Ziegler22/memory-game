16035c4920bb461c247b7467752e2542bdad6067
MrBurp <andreass.ziegler@gmail.com>
2022-06-02 19:29:28 +0200

    firstCard and secondCar working

--- src/js/Game.ts
+++ src/js/Game.ts
@@ -3,11 +3,35 @@ import { Card } from "./Card";
 export class Game {
   constructor(private dom: HTMLDivElement, private cards: Card[]) {}
 
+  private firstCard: HTMLDivElement | null = null;
+  private secondCard: HTMLDivElement | null = null;
+
+  reset() {
+    this.firstCard?.classList.remove("show");
+    this.secondCard?.classList.remove("show");
+    this.firstCard = null;
+    this.secondCard = null;
+  }
+
   addListeners() {
     Array.from(this.dom.querySelectorAll("[data-card]")).forEach((card, i) => {
       card.addEventListener("click", (e) => {
-        const cardClicked = e.currentTarget as HTMLDivElement;
-        cardClicked.classList.add("show");
+        if (!this.firstCard) {
+          this.firstCard = e.currentTarget as HTMLDivElement;
+          this.firstCard.classList.add("show");
+        } else if (!this.secondCard) {
+          this.secondCard = e.currentTarget as HTMLDivElement;
+          this.secondCard.classList.add("show");
+
+          if (
+            this.firstCard.getAttribute("data-card") ===
+            this.secondCard.getAttribute("data-card")
+          ) {
+            console.log("score");
+          } else {
+            setTimeout(this.reset.bind(this), 2000);
+          }
+        }
       });
     });
   }
